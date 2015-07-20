08df847919742e869940495be9549295806b1861
cnaegele <cnaegele@lausanne.ch>
2015-07-20 14:57:21 +0200

    1er .py modifié

--- test01.py
+++ test01.py
@@ -1,6 +1,5 @@
-Python 3.4.3 (default, Mar 26 2015, 22:07:01) 
-[GCC 4.9.2] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> print ('cool')
-cool
->>> 
+print ('cool')
+a=0
+for i in range(10):
+	a += i
+print a
